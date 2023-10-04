b9554d32d5cb37aa2b92e00c6596638076ba0c09
Matheus Meloni <63019098+MatMeloni@users.noreply.github.com>
2023-10-04 19:30:42 -0300

    Add files via upload

---
+++ #Sequencia Lab 7 - Algoritmos e Programa.py
@@ -0,0 +1,14 @@
+#Sequencia Lab 7 - Algoritmos e Programação I
+import math as m
+n=int(input('Valor de n: '))
+while n<=0:
+    print('Valor invalido')
+    n=int(input('Valor de n: '))
+x=float(input('Valo de x: '))
+while x==0:
+    print('Valor Invalido')
+    x=float(input('Valor de x:'))
+s=0
+for i in range(1,n+1,1):
+    s= s + ((3*i-1)*(x*i)/(i+2))
+print('Somatorio igual a : ',s)
